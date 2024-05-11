2aa3fb74f2e133b35c5d7cf01d6cc9ebc128894d
Angela Hempkmeier <aggsdpz@gmail.com>
2024-05-11 20:27:11 -0300

    feat: initial commit

---
+++ src/main/java/com/angiehempkmeier/studydiary/StudydiaryApplication.java
@@ -0,0 +1,13 @@
+package com.angiehempkmeier.studydiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudydiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudydiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angiehempkmeier/studydiary/StudydiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angiehempkmeier.studydiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudydiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
