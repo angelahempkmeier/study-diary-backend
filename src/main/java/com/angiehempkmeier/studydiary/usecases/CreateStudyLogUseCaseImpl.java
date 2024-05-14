a0849b83cee77ab4db627f5eeefcf9eab3b03797
Angela Hempkmeier <aggsdpz@gmail.com>
2024-05-13 21:38:41 -0300

    feat: creating implementation of study log use case

---
+++ src/main/java/com/angiehempkmeier/studydiary/usecases/CreateStudyLogUseCaseImpl.java
@@ -0,0 +1,13 @@
+package com.angiehempkmeier.studydiary.usecases;
+
+import com.angiehempkmeier.studydiary.adapters.input.CreateStudyLogRequest;
+import com.angiehempkmeier.studydiary.entities.StudyLog;
+
+import java.util.Date;
+
+public class CreateStudyLogUseCaseImpl implements CreateStudyLogUseCase {
+    @Override
+    public StudyLog execute(CreateStudyLogRequest request) {
+        return null;
+    }
+}
