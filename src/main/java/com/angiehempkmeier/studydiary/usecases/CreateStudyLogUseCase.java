69cbbac6128926133c8d938ee72618b33f7f8c2c
Angela Hempkmeier <aggsdpz@gmail.com>
2024-05-13 21:37:17 -0300

    feat: add map struct

--- src/main/java/com/angiehempkmeier/studydiary/usecases/CreateStudyLogUseCase.java
+++ src/main/java/com/angiehempkmeier/studydiary/usecases/CreateStudyLogUseCase.java
@@ -1,11 +1,12 @@
 package com.angiehempkmeier.studydiary.usecases;
 
+import com.angiehempkmeier.studydiary.adapters.input.CreateStudyLogRequest;
 import com.angiehempkmeier.studydiary.entities.StudyLog;
 
 import java.util.Date;
 
 public interface CreateStudyLogUseCase {
-    StudyLog createStudyLog(Date date, String description);
+    StudyLog execute(CreateStudyLogRequest request);
 
 
 }
